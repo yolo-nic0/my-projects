7502b21e499c0609820cf34c5cf7d8100636e396
Nico <142056865+yolo-nic0@users.noreply.github.com>
2023-08-29 03:38:34 +0300

    Delete config directory

--- config/constants.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-define('EMAIL', '');
-define('PASSWORD', '');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'user verification');
